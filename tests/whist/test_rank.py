c428d4e31e7399eafc16e1b1ddf32eab3fed3e24
Segelzwerg <marcel.haas@hhu.de>
2021-04-22 18:08:31 +0200

    ADD: test for short labels

--- tests/whist/test_rank.py
+++ tests/whist/test_rank.py
@@ -8,6 +8,10 @@ class RankTestCase(unittest.TestCase):
         jack = Rank.by_label('jack')
         self.assertEqual(Rank.J, jack)
 
+    def test_by_short_label(self):
+        jack = Rank.by_label('J', search_short_labels=True)
+        self.assertEqual(Rank.J, jack)
+
     def test_by_label_wrong_key(self):
         with self.assertRaises(KeyError):
             Rank.by_label('jones')
