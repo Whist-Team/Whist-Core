a8c7f4824b54e3fdceae6edbdf7b64005b914229
Segelzwerg <marcel.haas@hhu.de>
2021-05-11 15:37:05 +0200

    ADD: tests for token class

---
+++ tests/whist/core/test_token.py
@@ -0,0 +1,33 @@
+import asyncio
+import unittest
+from datetime import timedelta, datetime
+
+from jose import jwt
+
+from whist.core import SECRET_KEY, ALGORITHM
+from whist.core.player import Player, User
+from whist.core.token import Token
+
+USERNAME = 'honk'
+
+
+class TokenTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.payload = dict(sub=USERNAME)
+        user = User(user_id=1, username=USERNAME)
+        user_dict: dict = user.dict()
+        user_dict.update({'level': 1})
+        self.player = Player(**user_dict)
+        self.db = {USERNAME: self.player}
+        self.token = Token.create(self.payload, expires_delta=timedelta(minutes=2))
+
+    def test_create(self):
+        payload = jwt.decode(self.token, SECRET_KEY, algorithms=[ALGORITHM])
+        self.assertEqual(USERNAME, payload['sub'])
+        self.assertLess(datetime.utcnow(), datetime.fromtimestamp(payload['exp']))
+
+    def test_get_user(self):
+        loop = asyncio.get_event_loop()
+        user = loop.run_until_complete(Token.get_user(self.db, self.token))
+        loop.close()
+        self.assertEqual(self.player, user)
