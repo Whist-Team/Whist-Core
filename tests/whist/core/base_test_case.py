201fe702d6967f63b9006fc15db70b876f8c802c
Marcel <marcel.haas@hhu.de>
2021-05-12 13:47:49 +0200

    REFACTOR: format

--- tests/whist/core/base_test_case.py
+++ tests/whist/core/base_test_case.py
@@ -7,5 +7,5 @@ USERNAME = 'honk'
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.player = Player(**{'user_id':1, 'username':USERNAME, 'level':1})
+        self.player = Player(**{'user_id': 1, 'username': USERNAME, 'level': 1})
         self.db = {USERNAME: self.player}
