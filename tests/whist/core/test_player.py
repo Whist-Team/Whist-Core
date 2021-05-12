c6613ecf9439d773ac445c001bb71a01039ce912
Segelzwerg <marcel.haas@hhu.de>
2021-05-12 13:02:12 +0200

    ADD: player test case

---
+++ tests/whist/core/test_player.py
@@ -0,0 +1,12 @@
+from tests.whist.core.base_test_case import BaseTestCase, USERNAME
+from whist.core.player import Player
+
+
+class PlayerTestCase(BaseTestCase):
+    def test_get_player(self):
+        player = Player.get_player(self.db, USERNAME)
+        self.assertIsNotNone(player)
+
+    def test_not_found(self):
+        player = Player.get_player(self.db, 'abc')
+        self.assertIsNone(player)
