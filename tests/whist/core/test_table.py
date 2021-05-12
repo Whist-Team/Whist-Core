a6bc0fbff47e1798c5aaf21941bc38dfb8838966
Segelzwerg <marcel.haas@hhu.de>
2021-05-12 14:18:58 +0200

    ADD: tests for table

---
+++ tests/whist/core/test_table.py
@@ -0,0 +1,21 @@
+from tests.whist.core.base_test_case import BaseTestCase
+from whist.core.table import Table
+
+
+class TableTestCase(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.table = Table(session_id=1, min_player=1, max_player=4)
+
+    def test_ready(self):
+        self.table.join(self.player)
+        self.assertTrue(self.table.ready)
+
+    def test_join(self):
+        self.table.join(self.player)
+        self.assertEqual(1, len(self.table.users))
+
+    def test_leave(self):
+        self.table.join(self.player)
+        self.table.leave(self.player)
+        self.assertEqual(0, len(self.table.users))
