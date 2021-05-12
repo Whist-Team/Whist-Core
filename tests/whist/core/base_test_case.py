eda58304e9161ef00fa84197d3332b69a4624d98
Marcel <marcel.haas@hhu.de>
2021-05-12 13:44:48 +0200

    REFACTOR: create player directly

--- tests/whist/core/base_test_case.py
+++ tests/whist/core/base_test_case.py
@@ -1,15 +1,11 @@
 import unittest
 
 from whist.core.player import Player
-from whist.core.user import User
 
 USERNAME = 'honk'
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        user = User(user_id=1, username=USERNAME)
-        user_dict: dict = user.dict()
-        user_dict.update({'level': 1})
-        self.player = Player(**user_dict)
+        self.player = Player(**{'user_id':1, 'username':USERNAME, 'level':1})
         self.db = {USERNAME: self.player}
