a70aab9e815028f896150af1bc9a7a37998f6c53
Segelzwerg <marcel.haas@hhu.de>
2021-05-13 12:11:22 +0200

    UPDATE: use custom class for users

--- whist/core/session.py
+++ whist/core/session.py
@@ -1,7 +1,7 @@
 """DTO of a game room"""
 from pydantic import BaseModel
 
-from whist.core.player import Player
+from whist.core.userlist import UserList
 
 
 class Session(BaseModel):
@@ -9,4 +9,7 @@ class Session(BaseModel):
     User can join to play a game of Whist.
     """
     session_id: int
-    users: list[Player] = []
+    users: UserList = []
+
+    class Config:
+        arbitrary_types_allowed = True
