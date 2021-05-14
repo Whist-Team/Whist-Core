3ccb67f696fdcb55d61f62f2b807527a17b1c86c
Segelzwerg <marcel.haas@hhu.de>
2021-05-14 13:30:01 +0200

    UPDATE: iterable and subscriptable

--- whist/core/scoring/score.py
+++ whist/core/scoring/score.py
@@ -7,6 +7,13 @@ class Score:
     def __init__(self, players: list[Player], scores: list[int]):
         self.add_score(players, scores)
 
+    def __getitem__(self, item):
+        return self.tick_score[item]
+
+    def __iter__(self):
+        for player in self.tick_score:
+            yield player
+
     def add_score(self, players: list[Player], scores: list[int]) -> None:
         for player, score in zip(players, scores):
             self.tick_score.update({player: score})
