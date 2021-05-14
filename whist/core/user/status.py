b25e8c24b30b6ecde96cb9cde09ec4e8f3de0f1f
Segelzwerg <marcel.haas@hhu.de>
2021-05-14 02:00:49 +0200

    ADD: documentation

--- whist/core/user/status.py
+++ whist/core/user/status.py
@@ -1,3 +1,13 @@
+"""
+Data wrapper of player stati.
+"""
+
+
+# Wrapper class
+# pylint: disable=too-few-public-methods
 class Status:
+    """
+    Player status a table.
+    """
     ready: bool = False
     team: int = None
