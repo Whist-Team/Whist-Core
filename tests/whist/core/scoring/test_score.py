5c78cca79a16eba8853f457a3fc65ad0f39f127d
Segelzwerg <marcel.haas@hhu.de>
2021-05-15 15:41:20 +0200

    UPDATE: adapt test to teams

--- tests/whist/core/scoring/test_score.py
+++ tests/whist/core/scoring/test_score.py
@@ -1,14 +1,9 @@
-import unittest
-
-from whist.core.player import Player
 from whist.core.scoring.score import Score
+from whist.core.scoring.scoring_base_test_case import ScoringBaseTestCase
 
 
-class ScoreTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.player_a = Player(user_id=2, username='a', rating=1600)
-        self.player_b = Player(user_id=3, username='b', rating=1800)
+class ScoreTestCase(ScoringBaseTestCase):
 
     def test_score(self):
-        score = Score([self.player_a, self.player_b], [7, 6])
-        self.assertTrue(score[self.player_a] > score[self.player_b])
+        score = Score([self.team_a, self.team_b], [7, 6])
+        self.assertTrue(score[self.team_a] > score[self.team_b])
