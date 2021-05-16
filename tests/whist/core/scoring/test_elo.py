5bbe1a80542875b798971c9e4494b92e58c2d766
Segelzwerg <marcel.haas@hhu.de>
2021-05-16 20:58:07 +0200

    UPDATE: use teams in scoring tests

--- tests/whist/core/scoring/test_elo.py
+++ tests/whist/core/scoring/test_elo.py
@@ -1,33 +1,32 @@
-import unittest
-
-from whist.core.player import Player
+from tests.whist.core.scoring.scoring_base_test_case import ScoringBaseTestCase
 from whist.core.scoring.elo import EloRater
 from whist.core.scoring.score import Score
 from whist.core.scoring.score_card import ScoreCard
 
 
-class EloRaterTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.player_a = Player(user_id=2, username='a', rating=1600)
-        self.player_b = Player(user_id=3, username='b', rating=1800)
+class EloRaterTestCase(ScoringBaseTestCase):
 
     def test_rate_one_game(self):
         score_card = ScoreCard()
-        score = Score([self.player_a, self.player_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [7, 6])
         score_card.add_score(score)
-        EloRater.rate([self.player_a, self.player_b], score_card)
-        self.assertEqual(1630, self.player_a.rating)
-        self.assertEqual(1770, self.player_b.rating)
+        EloRater.rate([self.team_a, self.team_b], score_card)
+        self.assertEqual(1620, self.team_a.players[0].rating)
+        self.assertEqual(1820, self.team_a.players[1].rating)
+        self.assertEqual(1680, self.team_b.players[0].rating)
+        self.assertEqual(1680, self.team_b.players[1].rating)
 
     def test_rate_multiple_games(self):
         score_card = ScoreCard()
-        score = Score([self.player_a, self.player_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [7, 6])
         score_card.add_score(score)
         score_card.add_score(score)
-        score = Score([self.player_a, self.player_b], [6, 7])
+        score = Score([self.team_a, self.team_b], [6, 7])
         score_card.add_score(score)
 
-        EloRater.rate([self.player_a, self.player_b], score_card)
+        EloRater.rate([self.team_a, self.team_b], score_card)
 
-        self.assertLess(1600, self.player_a.rating)
-        self.assertGreater(1800, self.player_b.rating)
+        self.assertLess(1600, self.team_a.players[0].rating)
+        self.assertLess(1800, self.team_a.players[1].rating)
+        self.assertGreater(1700, self.team_b.players[0].rating)
+        self.assertGreater(1700, self.team_b.players[1].rating)
--- tests/whist/core/scoring/test_score_card.py
+++ tests/whist/core/scoring/test_score_card.py
@@ -1,33 +1,14 @@
-import unittest
-
-from whist.core.player import Player
+from tests.whist.core.scoring.scoring_base_test_case import ScoringBaseTestCase
 from whist.core.scoring.score import Score
 from whist.core.scoring.score_card import ScoreCard
 
 
-class ScoreCardTestCase(unittest.TestCase):
+class ScoreCardTestCase(ScoringBaseTestCase):
     def setUp(self) -> None:
+        super().setUp()
         self.score_card = ScoreCard()
-        self.player_a = Player(user_id=2, username='a', rating=1200)
-        self.player_b = Player(user_id=3, username='b', rating=1400)
 
     def test_add_score(self):
-        score = Score([self.player_a, self.player_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [7, 6])
         self.score_card.add_score(score)
         self.assertEqual(1, len(self.score_card))
-
-    def test_num_against_opp(self):
-        score = Score([self.player_a, self.player_b], [7, 6])
-        self.score_card.add_score(score)
-        self.score_card.add_score(score)
-        self.score_card.add_score(score)
-        self.assertEqual(3, self.score_card.num_against_opp(self.player_a, self.player_b))
-
-    def test_score_against_opp(self):
-        score = Score([self.player_a, self.player_b], [7, 6])
-        self.score_card.add_score(score)
-        self.score_card.add_score(score)
-        score = Score([self.player_a, self.player_b], [6, 7])
-        self.score_card.add_score(score)
-        self.assertEqual(2, self.score_card.score_against_opp(self.player_a, self.player_b))
-        self.assertEqual(1, self.score_card.score_against_opp(self.player_b, self.player_a))
