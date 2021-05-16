7e9f8512c36d91d70b5447d214660f3543bbc032
Segelzwerg <marcel.haas@hhu.de>
2021-05-16 21:04:14 +0200

    ADD: test for eq and str

--- tests/whist/core/test_player.py
+++ tests/whist/core/test_player.py
@@ -10,3 +10,16 @@ class PlayerTestCase(BaseTestCase):
     def test_not_found(self):
         player = Player.get_player(self.db, 'abc')
         self.assertIsNone(player)
+
+    def test_equal(self):
+        player = Player.get_player(self.db, USERNAME)
+        self.assertEqual(player, player)
+
+    def test_not_equal(self):
+        player = Player.get_player(self.db, USERNAME)
+        other = Player(user_id=2, username='other', rating=2000)
+        self.assertNotEqual(player, other)
+
+    def test_username(self):
+        player = Player.get_player(self.db, USERNAME)
+        self.assertEqual(player.username, str(player))
