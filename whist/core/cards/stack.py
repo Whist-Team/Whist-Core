1fcccb4a20ec9b7fb7a08aa670865fb48f8ec517
Segelzwerg <marcel.haas@hhu.de>
2021-06-11 23:48:53 +0200

    ADD: turn getter and safety double add

--- whist/core/cards/stack.py
+++ whist/core/cards/stack.py
@@ -1,4 +1,5 @@
 """Collection of cards"""
+
 from whist.core.cards.card import Card, Suit
 
 
@@ -14,6 +15,8 @@ class Stack:
         Put card on top of stack
         :param card: a Card placed on top of the stack
         """
+        if card in self.__cards:
+            raise KeyError(f'{card} already in deck')
         self.__cards.append(card)
 
     def winner_card(self, trump: Suit) -> Card:
@@ -28,6 +31,18 @@ class Stack:
         if len(winner_suit_cards) == 0:
             winner_suit_cards: list[Card] = self._card_of_suit(self.__cards[0].suit)
         return max(winner_suit_cards, key=lambda x: x.rank)
+
+    def get_turn(self, card: Card) -> int:
+        """
+        Gets the turn of a card played.
+        :param card: for which the turn number shall be found
+        :type card: Card
+        :return: the index of the card in the stack. 0 is the first the card played
+        :rtype: int
+        """
+        if card not in self.__cards:
+            raise KeyError(f'{card} is not in stack.')
+        return self.__cards.index(card)
 
     def _card_of_suit(self, trump):
         return [card for card in self.__cards if card.suit == trump]
