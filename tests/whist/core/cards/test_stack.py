d3bcc0a3e33e2d2cc355bc5bd3b2be3b242acece
Segelzwerg <marcel.haas@hhu.de>
2021-06-11 23:47:27 +0200

    FIX: setup

--- tests/whist/core/cards/test_stack.py
+++ tests/whist/core/cards/test_stack.py
@@ -5,16 +5,17 @@ from whist.core.cards.stack import Stack
 
 
 class StackTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.king_hearts = Card(Suit.HEARTS, Rank.K)
-        self.ace_hearts = Card(Suit.HEARTS, Rank.A)
-        self.seven_club = Card(Suit.CLUBS, Rank.NUM_7)
-        self.ten_diamond = Card(Suit.DIAMONDS, Rank.NUM_10)
-        self.stack = Stack()
-        self.stack.add(self.king_hearts)
-        self.stack.add(self.seven_club)
-        self.stack.add(self.ten_diamond)
-        self.stack.add(self.ace_hearts)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.king_hearts = Card(Suit.HEARTS, Rank.K)
+        cls.ace_hearts = Card(Suit.HEARTS, Rank.A)
+        cls.seven_club = Card(Suit.CLUBS, Rank.NUM_7)
+        cls.ten_diamond = Card(Suit.DIAMONDS, Rank.NUM_10)
+        cls.stack = Stack()
+        cls.stack.add(cls.king_hearts)
+        cls.stack.add(cls.seven_club)
+        cls.stack.add(cls.ten_diamond)
+        cls.stack.add(cls.ace_hearts)
 
     def test_winner_suit(self):
         self.assertEqual(self.ace_hearts, self.stack.winner_card(Suit.SPADES))
