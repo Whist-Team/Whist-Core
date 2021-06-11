d4f0fd8254ea3ccf0b14722150c44b06be9ebe0b
Segelzwerg <marcel.haas@hhu.de>
2021-06-12 00:56:45 +0200

    FIX: setup

--- tests/whist/core/team_base_test_case.py
+++ tests/whist/core/team_base_test_case.py
@@ -11,5 +11,7 @@ class TeamBaseTestCase(unittest.TestCase):
         cls.player_b = Player(user_id=3, username='b', rating=1800)
         cls.player_c = Player(user_id=4, username='c', rating=1700)
         cls.player_d = Player(user_id=5, username='d', rating=1700)
-        cls.team_a = Team(players=[cls.player_a, cls.player_b])
-        cls.team_b = Team(players=[cls.player_c, cls.player_d])
+
+    def setUp(self) -> None:
+        self.team_a = Team(players=[self.player_a, self.player_b])
+        self.team_b = Team(players=[self.player_c, self.player_d])
