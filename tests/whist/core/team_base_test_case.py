cb6e5f8c1f541796bc772b9087fc0f28f024ab24
Segelzwerg <marcel.haas@hhu.de>
2021-06-12 00:00:08 +0200

    UPDATE: make players class field

--- tests/whist/core/team_base_test_case.py
+++ tests/whist/core/team_base_test_case.py
@@ -7,9 +7,9 @@ from whist.core.user.player import Player
 class TeamBaseTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        player_a = Player(user_id=2, username='a', rating=1600)
-        player_b = Player(user_id=3, username='b', rating=1800)
-        player_c = Player(user_id=4, username='c', rating=1700)
-        player_d = Player(user_id=5, username='d', rating=1700)
-        cls.team_a = Team(players=[player_a, player_b])
-        cls.team_b = Team(players=[player_c, player_d])
+        cls.player_a = Player(user_id=2, username='a', rating=1600)
+        cls.player_b = Player(user_id=3, username='b', rating=1800)
+        cls.player_c = Player(user_id=4, username='c', rating=1700)
+        cls.player_d = Player(user_id=5, username='d', rating=1700)
+        cls.team_a = Team(players=[cls.player_a, cls.player_b])
+        cls.team_b = Team(players=[cls.player_c, cls.player_d])
