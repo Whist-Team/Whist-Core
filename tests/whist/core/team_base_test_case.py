9b96096eb18de2792bee18be20d0d86e0d1350fa
Segelzwerg <marcel.haas@hhu.de>
2021-06-11 23:59:15 +0200

    REFACTOR: make class method

--- tests/whist/core/team_base_test_case.py
+++ tests/whist/core/team_base_test_case.py
@@ -5,10 +5,11 @@ from whist.core.user.player import Player
 
 
 class TeamBaseTestCase(unittest.TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         player_a = Player(user_id=2, username='a', rating=1600)
         player_b = Player(user_id=3, username='b', rating=1800)
         player_c = Player(user_id=4, username='c', rating=1700)
         player_d = Player(user_id=5, username='d', rating=1700)
-        self.team_a = Team(players=[player_a, player_b])
-        self.team_b = Team(players=[player_c, player_d])
+        cls.team_a = Team(players=[player_a, player_b])
+        cls.team_b = Team(players=[player_c, player_d])
