0bb44aed4723ef8a82392b4e997992684954b0f8
Segelzwerg <marcel.haas@hhu.de>
2021-06-13 18:56:51 +0200

    ADD: player at table class

---
+++ whist/core/game/player_at_table.py
@@ -0,0 +1,32 @@
+from whist.core.cards.hand import Hand
+from whist.core.user.player import Player
+
+
+class PlayerAtTable:
+    def __init__(self, player: Player, hand: Hand):
+        self._player = player
+        self._hand = hand
+
+    def __eq__(self, other):
+        if not isinstance(other, PlayerAtTable):
+            return False
+        return self._player == other._player
+
+    @property
+    def player(self):
+        """
+        Getter player instance.
+        :return: player instance
+        :rtype: Player
+        """
+
+        return self._player
+
+    @property
+    def hand(self) -> Hand:
+        """
+        Hand of the player
+        :return: hand of the player
+        :rtype: Hand
+        """
+        return self._hand
