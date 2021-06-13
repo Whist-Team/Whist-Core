9a473db5504c29311f2c6cdb37502d2322e0e32b
Segelzwerg <marcel.haas@hhu.de>
2021-06-13 20:14:41 +0200

    ADD: test for next trick

--- tests/whist/core/game/test_hand.py
+++ tests/whist/core/game/test_hand.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from tests.whist.core.player_table_base_test_case import PlayerAtTableBaseTestCase
 from whist.core.cards.card import Suit
 from whist.core.game.hand import Hand
@@ -15,6 +17,17 @@ class HandTestCase(PlayerAtTableBaseTestCase):
         for player in self.play_order:
             self.assertEqual(13, len(player.hand))
         self.assertIsInstance(first_trick, Trick)
+
+    def test_done_trick(self):
+        first_trick = self.hand.deal()
+        with patch('whist.core.game.legal_checker.LegalChecker.check_legal', return_value=True):
+            for player in self.play_order:
+                card = player.hand._Hand__cards.pop()
+                first_trick.play_card(player, card)
+        next_trick = self.hand.next_trick()
+        for player in self.play_order:
+            self.assertEqual(12, len(player.hand))
+        self.assertNotEqual(first_trick, next_trick)
 
     def test_not_done_trick(self):
         _ = self.hand.deal()
