c90fce5b8a400421084805e15b22f654c7d2fdfe
Segelzwerg <marcel.haas@hhu.de>
2021-06-13 19:39:29 +0200

    ADD: test for first trick

--- tests/whist/core/game/test_hand.py
+++ tests/whist/core/game/test_hand.py
@@ -1,6 +1,7 @@
 from tests.whist.core.player_table_base_test_case import PlayerAtTableBaseTestCase
 from whist.core.cards.card import Suit
 from whist.core.game.hand import Hand
+from whist.core.game.trick import Trick
 
 
 class HandTestCase(PlayerAtTableBaseTestCase):
@@ -9,3 +10,4 @@ class HandTestCase(PlayerAtTableBaseTestCase):
         first_trick = hand.deal()
         for player in self.play_order:
             self.assertEqual(13, len(player.hand))
+        self.assertIsInstance(first_trick, Trick)
