9520555eaa8ff5d1bf4e4c1316f89bba4f00d091
Segelzwerg <marcel.haas@hhu.de>
2021-06-13 19:50:08 +0200

    ADD: test for hand

--- tests/whist/core/game/test_hand.py
+++ tests/whist/core/game/test_hand.py
@@ -2,12 +2,21 @@ from tests.whist.core.player_table_base_test_case import PlayerAtTableBaseTestCa
 from whist.core.cards.card import Suit
 from whist.core.game.hand import Hand
 from whist.core.game.trick import Trick
+from whist.core.game.warnings import TrickNotDoneWarning
 
 
 class HandTestCase(PlayerAtTableBaseTestCase):
-    def test_next_trick(self):
-        hand = Hand(self.play_order, Suit.HEARTS)
-        first_trick = hand.deal()
+    def setUp(self):
+        super().setUp()
+        self.hand = Hand(self.play_order, Suit.HEARTS)
+
+    def test_first_trick(self):
+        first_trick = self.hand.deal()
         for player in self.play_order:
             self.assertEqual(13, len(player.hand))
         self.assertIsInstance(first_trick, Trick)
+
+    def test_not_done_trick(self):
+        _ = self.hand.deal()
+        with self.assertRaises(TrickNotDoneWarning):
+            _ = self.hand.next_trick()
