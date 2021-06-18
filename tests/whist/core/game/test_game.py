2e8dfe80df34798b8682f554ed009e69c14b2360
Segelzwerg <marcel.haas@hhu.de>
2021-06-18 21:13:57 +0200

    ADD: test for first hand created

--- tests/whist/core/game/test_game.py
+++ tests/whist/core/game/test_game.py
@@ -1,11 +1,10 @@
-from unittest import skip
-
 from tests.whist.core.team_base_test_case import TeamBaseTestCase
 from whist.core.game.game import Game
+from whist.core.game.hand import Hand
 
 
 class GameTestCase(TeamBaseTestCase):
-    @skip('Not done yet.')
-    def test_done(self):
-        game = Game(teams=[self.team_a, self.team_b])
-        self.assertTrue(game.done)
+    def test_first_hand(self):
+        game = Game([self.team_a, self.team_b])
+        current_hand = game.next_hand()
+        self.assertIsInstance(current_hand, Hand)
