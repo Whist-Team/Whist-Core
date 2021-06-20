61a33698bf641237fe4a33d206c05754c650cb56
Marcel <marcel.haas@hhu.de>
2021-06-20 21:41:47 +0200

    UPDATE: version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='whist-core',
-    version='0.0.1',
+    version='0.1.0rc1',
     author='Whist Team',
     description='Game implementation of Whist.',
     long_description=long_description,
