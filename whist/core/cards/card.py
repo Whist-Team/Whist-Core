d1f3b62cd254eee9b1cf561bd3cef92d90b9cbca
Marcel <marcel.haas@hhu.de>
2021-07-01 09:02:51 +0200

    REVERT: warning disable

--- whist/core/cards/card.py
+++ whist/core/cards/card.py
@@ -14,7 +14,7 @@ class _OrderedEnum(Enum):
 
         obj = object.__new__(cls)
         obj._value_ = value
-        # pylint: disable=protected-access, unused-private-member
+        # pylint: disable=protected-access
         obj.__ordinal = len(cls.__members__)
         return obj
 
