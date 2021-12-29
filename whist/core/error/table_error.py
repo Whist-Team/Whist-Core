3299fae69d4c8f4758d7909738ead574a8506b0e
Segelzwerg <marcel.haas@hhu.de>
2021-12-29 13:41:57 +0100

    ADD: test for not ready start

--- whist/core/error/table_error.py
+++ whist/core/error/table_error.py
@@ -11,3 +11,9 @@ class TeamFullError(Exception):
     """
     Thrown if the team is already full.
     """
+
+
+class PlayerNotJoinedError(Exception):
+    """
+    Raised if a player has not yet joined the table.
+    """
