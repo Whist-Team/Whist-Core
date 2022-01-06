9deb6c3c57488d311fe4121bfa216d70a02e03c6
Segelzwerg <marcel.haas@hhu.de>
2022-01-06 22:40:20 +0100

    ADD: test for first hand played

--- tests/whist/core/game/test_legal_checker.py
+++ tests/whist/core/game/test_legal_checker.py
@@ -6,6 +6,14 @@ from whist.core.game.legal_checker import LegalChecker
 
 
 class TestLegalCheckerCase(TestCase):
+    def test_first_card(self):
+        hand = Hand()
+        first_card = Card(Suit.CLUBS, Rank.A)
+        second_card = Card(Suit.CLUBS, Rank.K)
+        hand.add(second_card)
+
+        self.assertTrue(LegalChecker.check_legal(hand, first_card, None))
+
     def test_suit_served(self):
         hand = Hand()
         first_card = Card(Suit.CLUBS, Rank.A)
