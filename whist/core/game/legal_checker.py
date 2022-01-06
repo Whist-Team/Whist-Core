b245015260316178b3f6b27706e159d11d8be6d6
Segelzwerg <marcel.haas@hhu.de>
2022-01-06 22:36:54 +0100

    FIX: check if card suit is same as lead

--- whist/core/game/legal_checker.py
+++ whist/core/game/legal_checker.py
@@ -10,15 +10,19 @@ class LegalChecker:
     """
 
     @staticmethod
-    def check_legal(hand: Hand, lead: Card) -> bool:
+    def check_legal(hand: Hand, card: Card, lead: Card) -> bool:
         """
         Checks if move is legal.
         :param hand: of the current player
         :type hand: Hand
+        :param card: the card which should be played next
+        :type card: Card
         :param lead: the first played card
         :type lead: Card
         :return: True if legal else false
         :rtype: bool
         """
         first_card_played = lead is not None
+        if card.suit == lead.suit:
+            return True
         return not (first_card_played and hand.contains_suit(lead.suit))
