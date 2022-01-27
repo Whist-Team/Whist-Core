ebe1c581c8e4de9676eb39806dc3bd45182e1464
iTitus <iTitus@users.noreply.github.com>
2022-01-27 23:28:49 +0100

    cleanup

--- whist/core/cards/card.py
+++ whist/core/cards/card.py
@@ -106,8 +106,7 @@ class Card(BaseModel):
     # pylint: disable=too-few-public-methods
     class Config:
         """
-        Configuration class for base model to make it immutable and hashable. Additionally, use
-        builtin type for enum in dictionary generation.
+        Configuration class for base model to make it immutable and hashable.
         """
         frozen = True
 
