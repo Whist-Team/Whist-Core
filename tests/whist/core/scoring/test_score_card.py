3cc9b5743eacdf4a15f03c46c4b0c01422f6a97b
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-01-30 20:46:22 +0100

    FIX: use correct scoring system

--- tests/whist/core/scoring/test_score_card.py
+++ tests/whist/core/scoring/test_score_card.py
@@ -9,23 +9,23 @@ class ScoreCardTestCase(TeamBaseTestCase):
         self.score_card = ScoreCard()
 
     def test_add_score(self):
-        score = Score([self.team_a, self.team_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [1, 0])
         self.score_card.add_score(score)
         self.assertEqual(1, len(self.score_card))
 
     def test_draw(self):
-        score = Score([self.team_a, self.team_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [1, 0])
         self.score_card.add_score(score)
-        score = Score([self.team_a, self.team_b], [6, 7])
+        score = Score([self.team_a, self.team_b], [0, 1])
         self.score_card.add_score(score)
         self.assertEqual(0, self.score_card.won(self.team_a))
 
     def test_max(self):
-        score = Score([self.team_a, self.team_b], [7, 6])
+        score = Score([self.team_a, self.team_b], [1, 0])
         self.score_card.add_score(score)
-        score = Score([self.team_a, self.team_b], [6, 7])
+        score = Score([self.team_a, self.team_b], [0, 1])
         self.score_card.add_score(score)
-        score = Score([self.team_a, self.team_b], [6, 7])
+        score = Score([self.team_a, self.team_b], [0, 1])
         self.score_card.add_score(score)
         self.assertEqual(2, self.score_card.max)
 
