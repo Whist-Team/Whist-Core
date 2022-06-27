8e35dc2fbfe97037b60397e4cb9c9df6b70426b4
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-06-27 21:29:51 +0200

    ADD: game done warning
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist/core/game/warnings.py
+++ whist/core/game/warnings.py
@@ -1,6 +1,10 @@
 """Warnings during game phase."""
 
 
+class GameDoneWarning(Warning):
+    """Is raised when some requests action on a Game already done."""
+
+
 class TrickNotDoneWarning(Warning):
     """The current trick is not done yet."""
 
