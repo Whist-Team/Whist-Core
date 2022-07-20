9e7505c8a25209d776951b03fbf2045cb044630c
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-07-20 21:30:33 +0200

    ADD: parameter type hitns
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist_core/scoring/score_calculator.py
+++ whist_core/scoring/score_calculator.py
@@ -1,10 +1,12 @@
+from whist_core.game.hand import Hand
+from whist_core.game.play_order import PlayOrder
 from whist_core.scoring.score import Score
 from whist_core.scoring.team import Team
 
 
 class ScoreCalculator:
     @staticmethod
-    def calc_score(current_hand, play_order) -> Score:
+    def calc_score(current_hand: Hand, play_order: PlayOrder) -> Score:
         tricks_won = [0, 0]
         for trick in current_hand.tricks:
             winner = trick.winner
