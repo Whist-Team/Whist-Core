b244ab2c14cc0c866eaa1ba95e80d6598bde5fec
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-07-20 21:54:20 +0200

    ADD: documentation REFACTOR: parameter names and constant
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist_core/scoring/score_calculator.py
+++ whist_core/scoring/score_calculator.py
@@ -1,17 +1,32 @@
+"""Scoring util"""
 from whist_core.game.hand import Hand
 from whist_core.game.play_order import PlayOrder
 from whist_core.scoring.score import Score
 from whist_core.scoring.team import Team
 
+# A team score for each trick exceeding 6.
+TRICK_EXCESS_BASE = 6
 
+
+# pylint: disable=too-few-public-methods
 class ScoreCalculator:
+    """
+    Util class for score calculating.
+    """
+
     @staticmethod
-    def calc_score(current_hand: Hand, play_order: PlayOrder) -> Score:
+    def calc_score(hand: Hand, play_order: PlayOrder) -> Score:
+        """
+        Calculates the score at the end of the hand.
+        :param hand: the hand to be scored
+        :param play_order: the order of players
+        :return: Score
+        """
         tricks_won = [0, 0]
-        for trick in current_hand.tricks:
+        for trick in hand.tricks:
             winner = trick.winner
             tricks_won[winner.team] += 1
-        tricks_won = [max(0, trick_score - 6) for trick_score in tricks_won]
+        tricks_won = [max(0, trick_score - TRICK_EXCESS_BASE) for trick_score in tricks_won]
         players_by_team = [[], []]
         for player in play_order:
             players_by_team[player.team].append(player.player)
