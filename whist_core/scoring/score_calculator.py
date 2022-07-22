7872b287272b571f47cf2b2e4fc84ea2704202c0
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-07-22 22:04:58 +0200

    REFACTOR: extract logic
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- whist_core/scoring/score_calculator.py
+++ whist_core/scoring/score_calculator.py
@@ -3,6 +3,7 @@ from whist_core.game.hand import Hand
 from whist_core.game.play_order import PlayOrder
 from whist_core.scoring.score import Score
 from whist_core.scoring.team import Team
+from whist_core.scoring.util import get_players_by_team
 
 # A team score for each trick exceeding 6.
 TRICK_EXCESS_BASE = 6
@@ -22,14 +23,17 @@ class ScoreCalculator:
         :param play_order: the order of players
         :return: Score
         """
+        tricks_won = ScoreCalculator.count_winners(hand)
+        tricks_won = [max(0, trick_score - TRICK_EXCESS_BASE) for trick_score in tricks_won]
+        players_by_team = get_players_by_team(play_order)
+        teams = [Team(players=players) for players in players_by_team]
+        score = Score(teams=teams, scores=tricks_won)
+        return score
+
+    @staticmethod
+    def count_winners(hand):
         tricks_won = [0, 0]
         for trick in hand.tricks:
             winner = trick.winner
             tricks_won[winner.team] += 1
-        tricks_won = [max(0, trick_score - TRICK_EXCESS_BASE) for trick_score in tricks_won]
-        players_by_team = [[], []]
-        for player in play_order:
-            players_by_team[player.team].append(player.player)
-        teams = [Team(players=players) for players in players_by_team]
-        score = Score(teams=teams, scores=tricks_won)
-        return score
+        return tricks_won
