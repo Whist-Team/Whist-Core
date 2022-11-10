d49419a3af959f7d37bce087210a51fce6b5b3b0
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-10 21:37:26 +0100

    UPDATE: matcher have state
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_core/session/test_matcher.py
+++ tests/whist_core/session/test_matcher.py
@@ -1,7 +1,7 @@
 import unittest
 
 from whist_core.error.matcher_error import NotEnoughPlayersError
-from whist_core.session.matcher import RandomMatcher, RoundRobinMatcher, Matcher
+from whist_core.session.matcher import RandomMatcher, RoundRobinMatcher
 from whist_core.session.userlist import UserList
 from whist_core.user.player import Player
 
@@ -14,18 +14,20 @@ class MatchTestCase(unittest.TestCase):
         player_d = Player(user_id=5, username='d', rating=1)
         self.players = [player_a, player_b, player_c, player_d]
         self.user_list = UserList()
+        self.random_matcher = RandomMatcher(self.user_list)
+        self.robin_matcher = RoundRobinMatcher(self.user_list)
         for player in self.players:
             self.user_list.append(player)
 
     def test_random_distribute(self):
-        teams = RandomMatcher.distribute(2, 2, self.user_list)
+        teams = self.random_matcher.distribute(2, 2)
         self.assertEqual(2, len(teams[0].players))
         self.assertEqual(2, len(teams[1].players))
         self.assertEqual(2, self.user_list.team_size(0))
         self.assertEqual(2, self.user_list.team_size(1))
 
     def test_round_robin_distribute(self):
-        teams = RoundRobinMatcher.distribute(2, 2, self.user_list)
+        teams = self.robin_matcher.distribute(2, 2)
         self.assertEqual(teams[0].players[0], self.players[0])
         self.assertEqual(teams[1].players[0], self.players[1])
         self.assertEqual(teams[0].players[1], self.players[2])
@@ -35,17 +37,14 @@ class MatchTestCase(unittest.TestCase):
         user_list = UserList()
         user_list.append(self.players[0])
         user_list.append(self.players[1])
+        robin_matcher = RoundRobinMatcher(user_list)
         with self.assertRaises(NotEnoughPlayersError):
-            _ = RoundRobinMatcher.distribute(2, 2, user_list)
-
-    def test_abstract(self):
-        with self.assertRaises(NotImplementedError):
-            Matcher.distribute(1, 2, self.user_list)
+            _ = robin_matcher.distribute(2, 2)
 
     def test_zero_players_per_team(self):
         with self.assertRaises(ValueError):
-            _ = RoundRobinMatcher.distribute(num_teams=2, team_size=0, users=self.user_list)
+            _ = self.robin_matcher.distribute(num_teams=2, team_size=0)
 
     def test_zero_teams(self):
         with self.assertRaises(ValueError):
-            _ = RoundRobinMatcher.distribute(num_teams=0, team_size=2, users=self.user_list)
+            _ = self.robin_matcher.distribute(num_teams=0, team_size=2)
--- whist_core/session/matcher.py
+++ whist_core/session/matcher.py
@@ -14,15 +14,21 @@ class Matcher(abc.ABC):
     """
     Abstrakt class for player to teams matching.
     """
+    _users: UserList
 
-    @staticmethod
+    def __init__(self, users: UserList):
+        """
+        Constructor.
+        :param users: the user list at that table
+        """
+        self._users = users
+
     @abc.abstractmethod
-    def distribute(num_teams: int, team_size: int, users: UserList) -> list[Team]:
+    def distribute(self, num_teams: int, team_size: int) -> list[Team]:
         """
         Distributes cards according to subclass implementation.
         :param num_teams: the amount of teams
         :param team_size: how many players per team
-        :param users: the user list at that table
         :return: the list of teams with players distributed to them
         """
         raise NotImplementedError
@@ -41,29 +47,27 @@ class RoundRobinMatcher(Matcher):
     Distributes the players in the order of the user list.
     """
 
-    @staticmethod
-    def distribute(num_teams: int, team_size: int, users: UserList) -> list[Team]:
+    def distribute(self, num_teams: int, team_size: int) -> list[Team]:
         """
         Distributes one player to each team each round in order of the user list. Repeats until
         the user list is empty.
         :param num_teams: the amount of teams
         :param team_size: how many players per team
-        :param users: the user list at that table
         :return: the teams in round robin distribution
         """
         if team_size <= 0:
             raise ValueError('The team size must be positive.')
         if num_teams <= 0:
             raise ValueError('There must be at least one team.')
-        players = users.players
+        players = self._users.players
         if num_teams * team_size > len(players):
             raise NotEnoughPlayersError(f'{num_teams * team_size} of players are need, but only '
                                         f'{len(players)} have joined.')
         for _ in range(0, team_size):
             for team_id in range(0, num_teams):
-                users.change_team(players.pop(0), team_id)
+                self._users.change_team(players.pop(0), team_id)
 
-        return users.teams
+        return self._users.teams
 
 
 class RandomMatcher(Matcher):
@@ -71,17 +75,16 @@ class RandomMatcher(Matcher):
     Distributes the players randomly to teams.
     """
 
-    @staticmethod
-    def distribute(num_teams: int, team_size: int, users: UserList) -> list[Team]:
+    def distribute(self, num_teams: int, team_size: int) -> list[Team]:
         """
         For given parameter distributes the players to teams.
         :return: None
         :rtype: None
         """
-        players = users.players
+        players = self._users.players
         teams: list = list(range(0, team_size)) * num_teams
         for player in players:
             team_id = random.choice(teams)  # nosec random
-            users.change_team(player, team_id)
+            self._users.change_team(player, team_id)
             teams.remove(team_id)
-        return users.teams
+        return self._users.teams
