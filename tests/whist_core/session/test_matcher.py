25aa3834880b08f84eeadfdd3f6a36569a1d4008
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-25 19:53:28 +0100

    UPDATE: tests to allwo early start
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_core/session/test_matcher.py
+++ tests/whist_core/session/test_matcher.py
@@ -36,13 +36,26 @@ class MatchTestCase(unittest.TestCase):
         self.assertEqual(distribution[3].player_index, 3)
         self.assertEqual(distribution[3].team_id, 1)
 
+    def test_random_min_player_distribute(self):
+        user_list = UserList()
+        user_list.append(self.players[0])
+        user_list.append(self.players[1])
+        random_matcher = RandomMatcher(number_teams=2)
+        distribution = random_matcher.distribute(user_list)
+        self.assertEqual(distribution[0].player_index, 0)
+        self.assertEqual(distribution[0].team_id, 0)
+        self.assertEqual(distribution[1].player_index, 1)
+
     def test_round_robin_min_player_distribute(self):
         user_list = UserList()
         user_list.append(self.players[0])
         user_list.append(self.players[1])
-        random_matcher = RandomMatcher(number_teams=2, team_size=2)
-        with self.assertRaises(NotEnoughPlayersError):
-            _ = random_matcher.distribute(user_list)
+        robin_matcher = RoundRobinMatcher(number_teams=2, team_size=2)
+        distribution = robin_matcher.distribute(user_list)
+        self.assertEqual(distribution[0].player_index, 0)
+        self.assertEqual(distribution[0].team_id, 0)
+        self.assertEqual(distribution[1].player_index, 1)
+
 
     def test_second_round_robin(self):
         _ = self.robin_matcher.distribute(self.user_list)
