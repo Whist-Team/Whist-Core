9d1b84365e8091eb5751174df90d173cb542f48b
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-26 14:23:05 +0100

    REMOVE: dependency tests
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/whist_core/test_token.py
+++ tests/whist_core/test_token.py
@@ -1,11 +1,9 @@
 import asyncio
-from datetime import timedelta, datetime
+from datetime import timedelta
 
 import pytest
-from jose import jwt
 
 from tests.whist_core.base_test_case import USERNAME, BaseTestCase
-from whist_core import SECRET_KEY, ALGORITHM
 from whist_core.token import Token
 
 
@@ -14,11 +12,6 @@ class TokenTestCase(BaseTestCase):
         super().setUp()
         self.payload = dict(sub=USERNAME)
         self.token = Token.create(self.payload, expires_delta=timedelta(minutes=2))
-
-    def test_create(self):
-        payload = jwt.decode(self.token, SECRET_KEY, algorithms=[ALGORITHM])
-        self.assertEqual(USERNAME, payload['sub'])
-        self.assertLess(datetime.utcnow(), datetime.fromtimestamp(payload['exp']))
 
     def test_get_user(self):
         loop = asyncio.get_event_loop()
@@ -37,8 +30,3 @@ class TokenTestCase(BaseTestCase):
         token = Token.create(dict(sub='abc'))
         with self.assertRaises(ValueError):
             _ = await Token.get_user(self.db, token)
-
-    def test_expired_token(self):
-        token = Token.create(self.payload, expires_delta=timedelta(minutes=-1))
-        with self.assertRaises(jwt.ExpiredSignatureError):
-            _ = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
