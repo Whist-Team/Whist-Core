db7687cdc671afe0ff510acf6e35d89f3799fba4
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2024-03-30 20:19:30 +0100

    Update __init__.py

--- whist_core/__init__.py
+++ whist_core/__init__.py
@@ -3,7 +3,7 @@ Retrieves envs for the whist core module.
 """
 import os
 
-# remember to also update the version in pyproject.toml!
+# remember to also update the version in pyproject.toml and docs/source/conf.py!
 __version__ = '0.9.2'
 
 ALGORITHM = os.getenv('ALGORITHM', 'HS256')
